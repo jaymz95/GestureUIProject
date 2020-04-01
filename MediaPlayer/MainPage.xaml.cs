5c3f1e76c264f03fd3cdccef2de74f9430118917
jaymz95 <jamesmullarkey15@gmail.com>
2020-04-01 23:08:37 +0100

    Timer added for assistant output

--- MediaPlayer/MainPage.xaml.cs
+++ MediaPlayer/MainPage.xaml.cs
@@ -68,6 +68,53 @@ namespace MediaPlayer
                 () => DrawFaceBoxes(detectedFaces));
         }
 
+        //**********************************************************************************************************************
+        static System.Windows.Forms.Timer myTimer = new System.Windows.Forms.Timer();
+        static int alarmCounter = 1;
+        static bool exitFlag = false;
+
+        // This is the method to run when the timer is raised.
+        private static void TimerEventProcessor(Object myObject,
+                                                EventArgs myEventArgs)
+        {
+            myTimer.Stop();
+
+            // Displays a message box asking whether to continue running the timer.
+            if (MessageBox.Show("Continue running?", "Count is: " + alarmCounter,
+               MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                // Restarts the timer and increments the counter.
+                alarmCounter += 1;
+                myTimer.Enabled = true;
+            }
+            else
+            {
+                // Stops the timer.
+                exitFlag = true;
+            }
+        }
+
+        public static int Main()
+        {
+            /* Adds the event and the event handler for the method that will 
+               process the timer event to the timer. */
+            myTimer.Tick += new EventHandler(TimerEventProcessor);
+
+            // Sets the timer interval to 5 seconds.
+            myTimer.Interval = 5000;
+            myTimer.Start();
+
+            // Runs the timer, and raises the event.
+            while (exitFlag == false)
+            {
+                // Processes all the events in the queue.
+                Application.DoEvents();
+            }
+            return 0;
+        }
+
+        //*******************************************************
+
         private void DrawFaceBoxes(IReadOnlyList<DetectedFace> detectedFaces)
         {
             cvsFaceOverlay.Children.Clear();
@@ -77,16 +124,10 @@ namespace MediaPlayer
             Debug.WriteLine(detectedFaces.Count);
             if (file != null)
             {
-                if (detectedFaces.Count <= 0 || playCommand == false)// && mediaPlayer.MediaPlayer.CurrentStateChanged += MediaElementState.Paused)
+                if ((detectedFaces.Count <= 0 && playCommand == true) || playCommand == false)// && mediaPlayer.MediaPlayer.CurrentStateChanged += MediaElementState.Paused)
                 {
-                    if(playCommand == true && detectedFaces.Count <= 0)
-                    {
-                        mediaPlayer.MediaPlayer.Play();
-                    }
-                    else
-                    {
-                        mediaPlayer.MediaPlayer.Pause();
-                    }
+                    mediaPlayer.MediaPlayer.Pause();
+                    
                 }
                 else if (detectedFaces.Count > 0 && pauseCommand == false)
                 {
